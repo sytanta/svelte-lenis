71f237af157726939c622d02f6473f2b1eef6fd4
TA SY TAN <sytanta@yahoo.com>
2023-04-07 11:10:08 +0700

    Remove console log

--- src/lib/utils/dynamicImport.ts
+++ src/lib/utils/dynamicImport.ts
@@ -6,6 +6,5 @@ export async function dynamicImport(module: string, promise: Promise<any>) {
 }
 
 export async function waitForImports(modules: string[]) {
-	console.log('zzzzzzzz', Object.keys(imports));
 	return await Promise.all(modules.map((mod) => imports[mod]).filter(Boolean));
 }
